7d8a38c4e53cbc272dff50d18e5437e70a722787
Marzio95 <marziodellarocca14@gmail.com>
2022-05-18 16:22:39 +0200

    completo le migration, il model ed il seeder

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'postText',
+        'slug',
+    ];
+
+    public static function createSlug($originalString)
+    {
+        $baseSlug = Str::of($originalString)->slug('-');
+        $slug = $baseSlug;
+        $_i = 1;
+        while (self::where('slug', $slug)->first()) {
+            $slug = "$baseSlug.$_i";
+            $_i++;
+        }
+        return $slug;
+    }
 }
--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +11,15 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 100; $i++) {
+            $title = $faker->word(rand(4, 8), true);
+            Post::create([
+                'title' => $title,
+                'postText' => $faker->text(rand(100, 500)),
+                'slug' => Post::createSlug($title),
+            ]);
+        }
     }
 }
